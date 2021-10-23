ea2052fffa03649676f85f9b7a59fe194a6eb59f
shervin-glitch <shervin2234@gmail.com>
2021-10-23 05:02:50 -0700

    Commited

---
+++ ScriptTools.py
@@ -0,0 +1,7 @@
+class Tools:
+    def __init__(self , *args , **kwargs) -> str:
+        super(Tools , self).__init__(*args , **kwargs)
+
+        return ['Package Will Release Soon' , 0 , 1]
+
+Tools().__init__()
