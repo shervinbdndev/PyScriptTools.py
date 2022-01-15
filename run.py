d2156cdb600e8d5a19024b53851c5ef53566d410
Shervin <68682979+shervin-glitch@users.noreply.github.com>
2022-01-14 23:12:04 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-from ScriptTools import Tools
+from PyScriptTools import NetworkTools , OtherTools
 
 
 if __name__ == "__main__":
-    new_object = Tools()
-    print(new_object.ConvertToAscii(new_object.ShowLocalIP() , ["green" , "yellow"] , "left" , "shade"))
+    network_object = NetworkTools()
+    use_ascii = OtherTools()
+    print(use_ascii.ConvertToAscii(network_object.ShowLocalIP() , ["green" , "red"] , "left" , "shade"))
 
     """ Enjoy All Methods And Use Them EveryWhere ! ! ! """
     """ Author's Github : https://github.com/shervin-glitch """
